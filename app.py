dafd3e98b02d1b82c776e2eb7275de7a72eb1a73
emmau <emanuele.maurer@iits-consulting.de>
2023-08-16 20:32:16 +0200

    asd

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def sitemap_scrap():
     return jsonify({"message": "Processing started, check back later for results."})
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 # if __name__ == "__main__":
 #     app.run(debug=True)
